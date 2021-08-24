205aba8075ead2ad35142158534fa68640e5921b
yani82 <yani.iman1@gmail.com>
2021-08-23 23:45:07 -0400

    'inventories' is assigned a value but never used error & eslint rules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import React from "react"; 
 import Product from "./Product"; 
+import Item from "./components/Item"; 
 
 const inventories = [
   { name: "Batik sarong", price: "$80" },
@@ -16,22 +17,27 @@ class App extends React.Component {
 
   // property initializer 
   state = {
-    inventory: {
-    name: "Batik sarong",
-    price: "$80",
-    },
-  }
+    inventory: [
+      { name: "Batik sarong", price: "$80" },
+      { name: "Wau (traditional kite)", price: "$50" },
+      { name: "Painting", price: "$100" },
+      { name: "Beaded necklace", price: "$30" },
+      { name: "Beaded scarf", price: "$60" },
+      { name: "Batik drawstring bag", price: "$25" },
+    ],
+    category: "Handicraft", 
+  };
 
   render() {
+    // console.log(this.state); 
 
-    console.log(this.state); 
-
-    const inventoryElements = inventories.map((inventory) => {
+    const inventoryElements = this.state.inventories.map((inventory) => {
   return <Product key={inventory.name} inventory={inventory} />
   });
     return (
     <div className="App">
       <header className="App-header">
+        <Item subject={this.state.category} />
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Welcome to Malaysian Creations {this.state.inventory.name}!</p>
         <p>New collection of {this.state.inventory.name}</p>
---
+++ src/components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Item(props) {
+    console.log(props);
+    return <div>Hello</div>;
+}
+  
